4d2add91d4123c3e5877ccc3a2d3708e472190ed
Stelios <apollon_g1_ste@hotmail.com>
2019-01-30 21:31:35 +0000

    Add Simple User Accounts

--- misconduct_detection_project/settings.py
+++ misconduct_detection_project/settings.py
@@ -182,3 +182,7 @@ LOGGING = {
         }
     }
 }
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- misconduct_detection_project/urls.py
+++ misconduct_detection_project/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('misconduct_detection_app.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('misconduct_detection_app.urls')),
 ]
