d2cbbdec35d98267500bc941ebd3fd14937e72d6
Stelios <apollon_g1_ste@hotmail.com>
2019-01-31 19:08:52 +0000

    Fix host settings

--- misconduct_detection_project/settings.py
+++ misconduct_detection_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['s1509375.inf.ac..uk']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 's1509375.inf.ac.uk']
 
 # Application definition
 
