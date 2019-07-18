63f24879c17f3452e8a41333ad4e8692bae4c115
Artūrs Jānis Pētersons <arturs.j.petersons@testdevlab.com>
2019-07-18 09:33:49 +0100

    Redirect to site from signup if logged in

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('login/', views.LoginView.as_view(), name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,8 +3,12 @@ import shutil
 
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView as DjangoLogin
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import resolve_url
+from django.conf import settings
 
 from misconduct_detection_app.env_settings import get_session_paths, get_user_paths
 
@@ -25,3 +29,12 @@ class SignUp(generic.CreateView):
                 shutil.move(sess_path, user_path)
         
         return response
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(resolve_url(settings.LOGIN_REDIRECT_URL))
+        return super().get(request, *args, **kwargs)
+
+
+class LoginView(DjangoLogin):
+    redirect_authenticated_user = True
